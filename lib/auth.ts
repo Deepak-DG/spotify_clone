67340288cb7c3c2d94d60810ce466812cd776b25
Deepak-n-468 <109666253+Deepak-n-468@users.noreply.github.com>
2022-07-28 16:49:07 +0530

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from "next"
 import { handleClientScriptLoad } from 'next/script'
 import prisma from './prisma'
 
