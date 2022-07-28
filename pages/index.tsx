0034cc5e1853fc557177e810ba77d62b9e46ee9d
Deepak-n-468 <109666253+Deepak-n-468@users.noreply.github.com>
2022-07-28 17:03:03 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import React from 'react';
+//import Head from 'next/head'
+//import Image from 'next/image'
+//import styles from '../styles/Home.module.css'
+
+
 const Home = () => {
   return (
     <div>home</div>
